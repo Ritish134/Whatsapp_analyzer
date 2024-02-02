7125d4815ade5c945e54ae6c26ca68a364876cad
Ritish Srivastava <121374890+Ritish134@users.noreply.github.com>
2024-02-02 10:41:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import preprocessor
+import helper
+import matplotlib.pyplot as plt
+
+st.sidebar.title('Whatsapp chat analyser')
+
+uploaded_file = st.sidebar.file_uploader('Choose a file')
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode('utf-8')
+    data = data.replace('\u202f', '')
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    # fetch unique user
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, 'Overall')
+
+    selected_user = st.sidebar.selectbox('Show analysis wrt', user_list)
+
+    if st.sidebar.button('Show Analysis'):
+
+        num_messages, words, num_media_message, links = helper.fetch_stats(selected_user, df)
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header('Total Messages')
+            st.title(num_messages)
+        with col2:
+            st.header('Total Words')
+            st.title(words)
+        with col3:
+            st.header('Media Shared')
+            st.title(num_media_message)
+        with col4:
+            st.header('Links Shared')
+            st.title(links)
+
+        # finding the busiest users in the group(group level)
+        if selected_user == 'Overall':
+            x, new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+
+            st.title('Most Busy Users')
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+            with col2:
+                st.dataframe(new_df)
+
+            # WordCloud
+            st.title('WordCloud')
+            df_wc = helper.create_wordcloud(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.imshow(df_wc)
+            st.pyplot(fig)
+
+            # most common words
+            most_common_df = helper.most_common_words(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.barh(most_common_df[0], most_common_df[1])
+            plt.xticks(rotation='vertical')
+            st.title('Most Common Words')
+            st.pyplot(fig)
+
+            # Emoji analysis
+            emoji_df = helper.emoji_helper(selected_user, df)
+            st.title('Emoji Analysis')
+            st.dataframe(emoji_df)
+
+
